028ec99753880735f8861ce028754ab6ef416f68
Fray <kozar20000@mail.ru>
2022-07-12 20:26:49 +0300

    even numbers

---
+++ aTask1/Program.cs
@@ -0,0 +1,15 @@
+﻿int len = 10;
+int [] arr = new int[len];
+Random rnd = new Random();
+int even = 0;
+
+for (int i = 0; i < len; i++)
+{
+    arr[i] = rnd.Next(100, 1000);
+    Console.WriteLine(arr[i] + " ");
+    if (arr[i] % 2 == 0)
+    even++;
+}
+
+Console.WriteLine();
+Console.WriteLine("Even numbers {0}:", even);
