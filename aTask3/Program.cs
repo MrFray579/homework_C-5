a36de595882b3c97abbe9588d22845a3e10ffad7
Fray <kozar20000@mail.ru>
2022-07-13 19:23:18 +0300

    diff

---
+++ aTask3/Program.cs
@@ -0,0 +1,14 @@
+﻿double[] arr = new double[] {3, 56, 43, 70, 9, 15};
+double diff = 0;
+double max = arr[0], min = arr[0];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > max)
+    max = arr[i];
+    if (arr[i] < min)
+    min = arr[i];
+}
+
+diff = max - min;
+Console.WriteLine("difference between min and max " + diff);
