34e511a05d5a9939cf9f292da6a20b05d6b6bf80
Fray <kozar20000@mail.ru>
2022-07-13 19:07:12 +0300

    sum of odd numbers

---
+++ aTask2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Console.WriteLine("Введите длинну массива:");
+//int a = Convert.ToInt32(Console.ReadLine());
+int a = 10;
+int[] arr = new int[a];
+Random rnd = new Random();
+int odd = 0;
+
+for (int i = 0; i < a; i++)
+{
+    arr[i] = rnd.Next(1, 10);
+    Console.Write(arr[i] + " ");
+   
+    if (i % 2 != 0)
+    {
+    odd += arr[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine("sum of odd numbers {0} ", odd);
+
